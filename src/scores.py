254966d10ef3d8db608da377a7c871b95d8e943d
BernalFA <fabernal1230@gmail.com>
2026-01-05 15:55:43 +0100

    implement custom functions for calculation of scores

---
+++ src/scores.py
@@ -0,0 +1,46 @@
+import sys
+from pathlib import Path
+
+import pandas as pd
+from pandarallel import pandarallel
+from rdkit.Chem import PandasTools, QED, SpacialScore
+
+# Add path to RDKit contributions
+# taken from https://github.com/rdkit/rdkit/issues/2279
+from rdkit.Chem import RDConfig
+
+sys.path.append(RDConfig.RDContribDir)
+
+from NP_Score import npscorer
+
+# The Böttcher score was calculated using the implementation from the Forli group.
+# A clone of the original [repo](https://github.com/forlilab/bottchscore/tree/master) was used for it.
+HERE = Path.cwd()
+bottcher_path = HERE.parent.parent / "Downloads" / "bottchscore"
+
+sys.path.append(str(bottcher_path))
+
+from bottchscore3 import calculate_bottchscore_from_smiles
+
+fscore = npscorer.readNPModel()
+def score_np(mol):
+    return npscorer.scoreMol(mol, fscore)
+
+def get_scores(filepath):
+    df = pd.read_csv(filepath)
+    smi_col = df.columns[df.columns.str.contains("smiles")][0]
+    PandasTools.AddMoleculeColumnToFrame(df, smilesCol=smi_col)
+
+    if len(df) > 1000:
+        pandarallel.initialize(nb_workers=24, progress_bar=True)
+        df["NP_likeness"] = df["ROMol"].parallel_apply(score_np)
+        df["QED"] = df["ROMol"].parallel_apply(QED.default)
+        df["Boettcher"] = df[smi_col].parallel_apply(calculate_bottchscore_from_smiles)
+        df["nSPS"] = df["ROMol"].parallel_apply(SpacialScore.SPS)
+    else:
+        df["NP_likeness"] = df["ROMol"].apply(score_np)
+        df["QED"] = df["ROMol"].apply(QED.default)
+        df["Boettcher"] = df[smi_col].apply(calculate_bottchscore_from_smiles)
+        df["nSPS"] = df["ROMol"].apply(SpacialScore.SPS)
+    cols = [df.columns[0]] + ["NP_likeness", "QED", "Boettcher", "nSPS"]
+    return df[cols]
